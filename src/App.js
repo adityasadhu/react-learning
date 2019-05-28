3f02b7e3a5b64ca3baa9dfa6af41ff6346887f73
Aditya Sadhu <aditya.hf96@gmail.com>
2019-05-28 17:06:29 -0400

    check ssh keys

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import "./App.css";
 import FetchRandomUser from "./components/FetchRandomUser";
 
 export default class App extends Component {
-  state = {
-    visible: true
-  };
-
   render() {
-    const buttonText = "toggle counter";
-
     return (
       <div className="App">
         <FetchRandomUser />
