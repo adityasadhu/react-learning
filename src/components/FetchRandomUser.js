3e9b01cdbe625fde6e3aade0ad828a62d7391c0d
Aditya Sadhu <aditya.hf96@gmail.com>
2019-05-28 16:51:06 -0400

    render arrays in react

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -3,29 +3,35 @@ import React, { Component } from "react";
 export default class FetchRandomUser extends Component {
   state = {
     loading: true,
-    person: null
+    people: []
   };
 
   async componentDidMount() {
-    const url = "https://api.randomuser.me/";
+    const url = "https://api.randomuser.me/?results=5";
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ person: data.results[0], loading: false });
+    this.setState({ people: data.results, loading: false });
   }
 
   render() {
+    if (this.state.loading) {
+      return <div>loading..</div>;
+    }
+
+    if (!this.state.people) {
+      return <div>didnt recieve back people data </div>;
+    }
+
     return (
       <div>
-        {this.state.loading || !this.state.person ? (
-          <div>loading... </div>
-        ) : (
-          <div>
-            <div>{this.state.person.name.title}</div>
-            <div>{this.state.person.name.first}</div>
-            <div>{this.state.person.name.last}</div>
-            <img src={this.state.person.picture.large} />
+        {this.state.people.map(person => (
+          <div key={person.login.uuid}>
+            <div>{person.name.title}</div>
+            <div>{person.name.first}</div>
+            <div>{person.name.last}</div>
+            <img src={person.picture.large} />
           </div>
-        )}
+        ))}
       </div>
     );
   }
