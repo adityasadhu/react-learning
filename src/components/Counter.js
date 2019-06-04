03ef31c07331afdc0eb8854ea6d85c7507ca5825
Aditya Sadhu <aditya.hf96@gmail.com>
2019-06-04 00:09:12 -0400

    trying out useEffect in hooks

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Counter = () => {
   // state = {
@@ -6,12 +6,31 @@ const Counter = () => {
   // };
 
   const [count, setCount] = useState(0);
+  const [person, setPerson] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [countClicked, setCountClicked] = useState(0);
 
-  const increment = () => setCount(count + 1);
+  const increment = () => {
+    setCount(count + 1);
+    setCountClicked(countClicked + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+    setCountClicked(countClicked + 1);
+  };
 
-  const decrement = () => setCount(count - 1);
+  const reset = () => {
+    setCount(0);
+    setCountClicked(countClicked + 1);
+  };
 
-  const reset = () => setCount(0);
+  useEffect(async () => {
+    const response = await fetch("https://api.randomuser.me/");
+    const data = await response.json();
+    const [item] = data.results;
+    setPerson(item);
+    setLoading(false);
+  }, []);
 
   return (
     <div>
@@ -19,6 +38,8 @@ const Counter = () => {
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button>
       <button onClick={reset}> reset</button>
+      <p>{countClicked}</p>
+      {loading ? <div>loading... </div> : <div>{person.name.first}</div>}
     </div>
   );
 };
