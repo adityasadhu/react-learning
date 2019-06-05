f3a9d6b353ba3a565c14f56480b487589557e4f6
Aditya Sadhu <aditya.hf96@gmail.com>
2019-06-04 22:21:45 -0400

    use a better state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,41 +5,56 @@ const Counter = () => {
   //   count: 0
   // };
 
-  const [count, setCount] = useState(0);
-  const [person, setPerson] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [countClicked, setCountClicked] = useState(0);
+  // const [count, setCount] = useState(0);
+  // const [person, setPerson] = useState(null);
+  // const [loading, setLoading] = useState(true);
+  // const [countClicked, setCountClicked] = useState(0);
+
+  const [state, setState] = useState({
+    count: 0,
+    person: null,
+    loading: true,
+    countClicked: 0
+  });
 
   const increment = () => {
-    setCount(count + 1);
-    setCountClicked(countClicked + 1);
+    setState({
+      ...state,
+      count: state.count + 1,
+      countClicked: state.countClicked + 1
+    });
   };
   const decrement = () => {
-    setCount(count - 1);
-    setCountClicked(countClicked + 1);
+    setState({
+      ...state,
+      count: state.count - 1,
+      countClicked: state.countClicked + 1
+    });
   };
 
   const reset = () => {
-    setCount(0);
-    setCountClicked(countClicked + 1);
+    setState({ ...state, count: 0, countClicked: state.countClicked + 1 });
   };
 
   useEffect(async () => {
     const response = await fetch("https://api.randomuser.me/");
     const data = await response.json();
     const [item] = data.results;
-    setPerson(item);
-    setLoading(false);
+    setState({ ...state, person: item, loading: false });
   }, []);
 
   return (
     <div>
-      <div> count: {count} </div>
+      <div> count: {state.count} </div>
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button>
       <button onClick={reset}> reset</button>
-      <p>{countClicked}</p>
-      {loading ? <div>loading... </div> : <div>{person.name.first}</div>}
+      <p>{state.countClicked}</p>
+      {state.loading ? (
+        <div>loading... </div>
+      ) : (
+        <div>{state.person.name.first}</div>
+      )}
     </div>
   );
 };
