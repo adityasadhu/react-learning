c1f7d22c59a0b9ae52dcb5e6c167bd0fd9b671cb
Aditya Sadhu <aditya.hf96@gmail.com>
2019-06-10 23:27:54 -0400

    basic todo component with hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Counter from "./components/Counter";
+import Form from "./components/Form";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Counter />
-      </div>
+export default () => {
+  const [todos, setTodos] = useState([]);
+
+  const toggleComplete = i =>
+    setTodos(
+      todos.map((todo, k) =>
+        k === i
+          ? {
+              ...todo,
+              complete: !todo.complete
+            }
+          : todo
+      )
     );
-  }
-}
+
+  return (
+    <div className="App">
+      <Form
+        onSubmit={text => setTodos([{ text, complete: false }, ...todos])}
+      />
+      <div>
+        {todos.map(({ text, complete }, i) => (
+          <div
+            key={Math.random()}
+            onClick={() => toggleComplete(i)}
+            style={{
+              textDecoration: complete ? "line-through" : ""
+            }}
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => setTodos([])}> reset</button>
+    </div>
+  );
+};
